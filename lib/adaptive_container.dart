f2b051357d396747b306f449c14ee83c31574011
Zeus <hidarizeus@gmail.com>
2021-04-15 17:03:36 +0800

    remove flex

--- lib/adaptive_container.dart
+++ lib/adaptive_container.dart
@@ -7,10 +7,9 @@ import 'package:flutter/material.dart';
 class AdaptiveContainer extends StatelessWidget {
   final List<AdaptiveItem> children;
   final ScrollPhysics physics;
-  final bool isParent;
   final CrossAxisAlignment crossAxisAlignment;
   final MainAxisAlignment mainAxisAlignment;
-  AdaptiveContainer({this.isParent = false,required this.children, this.physics = const ClampingScrollPhysics(), this.crossAxisAlignment = CrossAxisAlignment.start, this.mainAxisAlignment = MainAxisAlignment.start});
+  AdaptiveContainer({required this.children, this.physics = const ClampingScrollPhysics(), this.crossAxisAlignment = CrossAxisAlignment.start, this.mainAxisAlignment = MainAxisAlignment.start});
 
   List<List<AdaptiveItem>> getContent(maxCount, List<AdaptiveItem> toCheck) {
     List<List<AdaptiveItem>> result = [];
@@ -29,107 +28,57 @@ class AdaptiveContainer extends StatelessWidget {
     }
     return result;
   }
-  // Widget childWidget(double width,Size _size) {
-  //   print(width);
-  //
-  // }
   @override
   Widget build(BuildContext context) {
     final Size _size = MediaQuery.of(context).size;
     return LayoutBuilder(
         builder: (context, constraint) {
           if(children.length > 0){
-            print(constraint.maxWidth);
             int maxChildCount = 2;
             if(constraint.maxWidth < 800) {
               maxChildCount = 1;
             }
             List<List<AdaptiveItem>> content = getContent(maxChildCount, List<AdaptiveItem>.from(children));
-            if(!isParent){
-              return Container(
-                width: _size.width,
-                child: Scrollbar(
-                  child: SingleChildScrollView(
-                      physics: physics,
-                      child: Column(
-                        children: [
-                          for(var colList in content)...{
-                            Container(
-                              width: constraint.maxWidth,
-                              child: Row(
-                                crossAxisAlignment: crossAxisAlignment,
-                                mainAxisAlignment: mainAxisAlignment,
-                                children: [
-                                  for(var child in colList)...{
-                                    if(constraint.maxWidth < 800)...{
-                                      Expanded(
-                                        child: child.asWidget,
-                                      )
-                                    } else
-                                      ...{
-                                        if(child.width != null)...{
-                                          child.asWidget
-                                        } else
-                                          ...{
-                                            Expanded(
-                                              child: child.asWidget,
-                                            )
-                                          }
-                                      }
-                                  }
-                                ],
-                              ),
-                            )
-                          }
-                        ],
-                      )
-                  ),
-                ),
-              );
-            }else{
-              return Expanded(
-                child: Container(
-                  width: _size.width,
-                  child: Scrollbar(
-                    child: SingleChildScrollView(
-                        physics: physics,
-                        child: Column(
-                          children: [
-                            for(var colList in content)...{
-                              Container(
-                                width: constraint.maxWidth,
-                                child: Row(
-                                  crossAxisAlignment: crossAxisAlignment,
-                                  mainAxisAlignment: mainAxisAlignment,
-                                  children: [
-                                    for(var child in colList)...{
-                                      if(constraint.maxWidth < 800)...{
-                                        Expanded(
-                                          child: child.asWidget,
-                                        )
+            return Container(
+              width: _size.width,
+              child: Scrollbar(
+                child: SingleChildScrollView(
+                    physics: physics,
+                    child: Column(
+                      children: [
+                        for(var colList in content)...{
+                          Container(
+                            width: constraint.maxWidth,
+                            child: Row(
+                              crossAxisAlignment: crossAxisAlignment,
+                              mainAxisAlignment: mainAxisAlignment,
+                              children: [
+                                for(var child in colList)...{
+                                  if(constraint.maxWidth < 800)...{
+                                    Expanded(
+                                      child: child.asWidget,
+                                    )
+                                  } else
+                                    ...{
+                                      if(child.width != null)...{
+                                        child.asWidget
                                       } else
                                         ...{
-                                          if(child.width != null)...{
-                                            child.asWidget
-                                          } else
-                                            ...{
-                                              Expanded(
-                                                child: child.asWidget,
-                                              )
-                                            }
+                                          Expanded(
+                                            child: child.asWidget,
+                                          )
                                         }
                                     }
-                                  ],
-                                ),
-                              )
-                            }
-                          ],
-                        )
-                    ),
-                  ),
+                                }
+                              ],
+                            ),
+                          )
+                        }
+                      ],
+                    )
                 ),
-              );
-            }
+              ),
+            );
           }
           return Container(
             width: _size.width,
